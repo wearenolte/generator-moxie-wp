cba267c8d7c5233eed35702898989eea642e0e1e
mitogh <mitogh@gmail.com>
2015-10-06 00:47:33 -0500

    Update package and retrive and setup an installation from a git

---
+++ install/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var path = require('path');
+var chmod = require('chmod');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var MoxieInstall = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+  prompts: function () {
+    var done = this.async();
+    this.log(yosay(
+      'Welcome to the Moxie Wordpress Set Up. ' +
+      'from a git repo'
+    ));
+
+    var prompts = [
+      {
+        name: 'repoURL',
+        message: 'Enter the url of the repository',
+        default: function(){
+          return '';
+        }
+      },
+    ];
+
+    this.prompt(prompts, function( props ){
+      this.repoURL = props.repoURL;
+      this.themeName = props.themeName;
+      done();
+    }.bind( this ));
+  },
+  writing: function(){
+    this.fs.copyTpl(
+      this.templatePath('script'),
+      this.destinationPath('../script.sh'),
+      {
+        repoURL: path.normalize( this.repoURL )
+      }
+    );
+  },
+  install: function(){
+    chmod( '../script.sh', 775 );
+    this.spawnCommand( '../script.sh').on('error', function(error){
+      console.log(error);
+    });
+  }
+});
+module.exports = MoxieInstall;
