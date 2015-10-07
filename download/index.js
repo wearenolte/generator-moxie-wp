3c6038cb7a5c607c20726a9a4c38facb9c442630
mitogh <mitogh@gmail.com>
2015-10-06 22:48:51 -0500

    Compose get generator to retirve a fresh installtion of WP

--- download/index.js
+++ download/index.js
@@ -1,8 +1,7 @@
+'use strict';
+
 var yeoman = require( 'yeoman-generator' );
-var https = require( 'https' );
-var fs = require('fs.extra');
 var chalk = require( 'chalk' );
-var rimraf = require( 'rimraf' );
 
 var MoxieDownload = yeoman.generators.Base.extend( {
     initializing: function () {
@@ -23,69 +22,10 @@ var MoxieDownload = yeoman.generators.Base.extend( {
         }
       }.bind( this));
     },
-    createTmpDirectory: function(){
-      var directory = './tmp';
-      console.log('\n' + chalk.bold.yellow('Creating temporarily directory'));
-
-      if(! fs.existsSync( directory ) ) {
-        fs.mkdirSync( directory );
-      } else {
-        console.log( chalk.yellow('Great, we already have one.') );
-      }
-    },
-    downloadWordpress: function(){
-      var url = 'https://wordpress.org/latest.zip'
-      var directory = './tmp';
-      var cb = this.async();
-      var message = 'Downloading & extracting WP in '+  directory + ' directory';
-
-      console.log('\n' + chalk.bold.yellow( message ));
-      this.extract(url, directory, cb);
-    },
-    removeDefaultWPContent: function(){
-      var cb = this.async();
-      var that = this;
-      var message = 'Removing default wp-content directory from Wordpress downloaded file';
-      console.log('\n' + chalk.bold.yellow( message ));
-
-      rimraf('./tmp/wordpress/wp-content', function(){
-        cb();
-      });
-    },
-    moveWordpressFiles: function(){
-      var finito = this.async();
-      var that = this;
-      console.log( chalk.bold.yellow( 'Copy all files from Wordpress to this directory' ) );
-      fs.copyRecursive('./tmp/wordpress', '.', function( error ) {
-        if( error ){
-          console.log( chalk.bold.red( 'There was an error where the files were copied' ) );
-        } else {
-          console.log( chalk.yellow( 'All files were copied succesfully' ) );
-        }
-        finito();
-      });
-    },
-    addPluginsDirectory: function(){
-      var contentDir = './wp-content';
-      var pluginsDir = contentDir + '/plugins';
-      var hasWpContentDir = fs.existsSync( contentDir );
-      var hasPluginsDirectory = fs.existsSync( pluginsDir );
-
-      console.log( chalk.bold.yellow( 'Create a plugins directory if has not' ) );
-      if( hasWpContentDir && ! hasPluginsDirectory ){
-          fs.mkdirSync( pluginsDir );
-      } else {
-        console.log( chalk.yellow('Looks like you do not have wp-content directory') );
-      }
-    },
-    removeTmpDirectory: function(){
-      var cb = this.async();
-      var message = 'Removing the temporarily directory';
-      console.log('\n' + chalk.bold.yellow( message ));
-
-      rimraf('./tmp', function(){
-        cb();
-      });
+    install: function(){
+      var done = this.async();
+      this.composeWith('moxie-wp:get');
+      done();
     },
     end: function(){
       var message = 'We are done, thanks, and have a nice day';
